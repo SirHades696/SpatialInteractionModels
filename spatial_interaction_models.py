48e902ab83fb649d3820ec109a543c6e29ecfb30
Emmanuel Nicanor <djnonasrm@gmail.com>
2023-08-14 12:04:21 -0600

    Added GUI events

--- spatial_interaction_models.py
+++ spatial_interaction_models.py
@@ -35,6 +35,8 @@ import os.path
 from qgis.core import *
 from PyQt5.QtWidgets import *
 from qgis.gui import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QSettings
 
 class SpatialInteractionModels:
     """QGIS Plugin Implementation."""
@@ -165,24 +167,45 @@ class SpatialInteractionModels:
         self.dlg.tipo_filt_fluj.setEnabled(False)
         self.dlg.val1_dist.setEnabled(False)
         self.dlg.val1_dist.setReadOnly(False)
+        self.dlg.val1_dist.clear()
+        self.dlg.val1_dist.setValidator(QDoubleValidator())
         self.dlg.val2_dist.setEnabled(False)
         self.dlg.val2_dist.setReadOnly(False)
+        self.dlg.val2_dist.clear()
+        self.dlg.val2_dist.setValidator(QDoubleValidator())
         self.dlg.val1_fluj.setEnabled(False)
         self.dlg.val1_fluj.setReadOnly(False)
+        self.dlg.val1_fluj.clear()
+        self.dlg.val1_fluj.setValidator(QDoubleValidator())
         self.dlg.val2_fluj.setEnabled(False)
         self.dlg.val2_fluj.setReadOnly(False)
-
+        self.dlg.val2_fluj.clear()
+        self.dlg.val2_fluj.setValidator(QDoubleValidator())
         self.dlg.val1_dist.setVisible(False)
         self.dlg.val2_dist.setVisible(False)
 
         self.dlg.val1_fluj.setVisible(False)
         self.dlg.val2_fluj.setVisible(False)
 
+
+
         #----------outputs
         self.dlg.sqlite_check_load.setVisible(False)
         self.dlg.geojson_check_load.setVisible(False)
         self.dlg.geopackage_check_load.setVisible(False)
         self.dlg.hd_check_load.setVisible(False)
+
+        self.dlg.sqlite_check.setChecked(False)
+        self.dlg.geojson_check.setChecked(False)
+        self.dlg.geopackage_check.setChecked(False)
+        self.dlg.hd_check.setChecked(False)
+
+        self.dlg.prefijo.clear()
+        self.dlg.output.clear()
+        self.dlg.check_projects.setChecked(False)
+        self.dlg.projects_combobox.clear()
+        self.dlg.projects_combobox.setEnabled(False)
+        self.load_projects()
 
 
     def connections(self):
@@ -201,6 +224,16 @@ class SpatialInteractionModels:
         self.dlg.tipo_filt_dist.currentIndexChanged.connect(self.restrictions_valuesD)
         self.dlg.tipo_filt_fluj.currentIndexChanged.connect(self.restrictions_valuesF)
 
+        # Output
+        self.dlg.sqlite_check.stateChanged.connect(self.hide_show_sqlite)
+        self.dlg.geojson_check.stateChanged.connect(self.hide_show_geojson)
+        self.dlg.geopackage_check.stateChanged.connect(self.hide_show_geopackage)
+        self.dlg.hd_check.stateChanged.connect(self.hide_show_hd)
+
+        self.dlg.check_projects.stateChanged.connect(self.recent_projects)
+        self.dlg.projects_combobox.currentIndexChanged.connect(self.set_project_output)
+        self.dlg.btn_output.clicked.connect(self.select_output)
+
     # ------ TABS
     def tab_inputs(self):
         self.dlg.tabWidget.setCurrentIndex(0)
@@ -216,20 +249,45 @@ class SpatialInteractionModels:
         if index != 0:
             if index == 1:
                 self.dlg.tipo_filt_dist.setEnabled(True)
+                self.dlg.tipo_filt_dist.setCurrentIndex(0)
                 self.dlg.tipo_filt_fluj.setEnabled(False)
+                self.dlg.tipo_filt_fluj.setCurrentIndex(0)
+                self.dlg.val1_dist.setVisible(False)
+                self.dlg.val2_dist.setVisible(False)
+                self.dlg.val1_fluj.setVisible(False)
+                self.dlg.val2_fluj.setVisible(False)
             elif index == 2:
                 self.dlg.tipo_filt_dist.setEnabled(False)
+                self.dlg.tipo_filt_dist.setCurrentIndex(0)
                 self.dlg.tipo_filt_fluj.setEnabled(True)
+                self.dlg.tipo_filt_fluj.setCurrentIndex(0)
+                self.dlg.val1_dist.setVisible(False)
+                self.dlg.val2_dist.setVisible(False)
+                self.dlg.val1_fluj.setVisible(False)
+                self.dlg.val2_fluj.setVisible(False)
             elif index == 3:
                 self.dlg.tipo_filt_dist.setEnabled(True)
+                self.dlg.tipo_filt_dist.setCurrentIndex(0)
                 self.dlg.tipo_filt_fluj.setEnabled(True)
+                self.dlg.tipo_filt_fluj.setCurrentIndex(0)
+                self.dlg.val1_dist.setVisible(False)
+                self.dlg.val2_dist.setVisible(False)
+                self.dlg.val1_fluj.setVisible(False)
+                self.dlg.val2_fluj.setVisible(False)
         else:
             self.dlg.tipo_filt_dist.setEnabled(False)
+            self.dlg.tipo_filt_dist.setCurrentIndex(0)
             self.dlg.tipo_filt_fluj.setEnabled(False)
+            self.dlg.tipo_filt_fluj.setCurrentIndex(0)
             self.dlg.val1_dist.setEnabled(False)
             self.dlg.val2_dist.setEnabled(False)
             self.dlg.val1_fluj.setEnabled(False)
             self.dlg.val2_fluj.setEnabled(False)
+            self.dlg.val1_dist.setVisible(False)
+            self.dlg.val2_dist.setVisible(False)
+            self.dlg.val1_fluj.setVisible(False)
+            self.dlg.val2_fluj.setVisible(False)
+
 
     def restrictions_valuesD(self):
         index = self.dlg.tipo_filt_dist.currentIndex()
@@ -237,13 +295,16 @@ class SpatialInteractionModels:
             if index == 1 or index == 2:
                 self.dlg.val1_dist.setEnabled(True)
                 self.dlg.val1_dist.setVisible(True)
+                self.dlg.val1_dist.setPlaceholderText("Ingresa un valor")
                 self.dlg.val2_dist.setEnabled(False)
                 self.dlg.val2_dist.setVisible(False)
             elif index == 3:
                 self.dlg.val1_dist.setEnabled(True)
                 self.dlg.val1_dist.setVisible(True)
+                self.dlg.val1_dist.setPlaceholderText("Valor inicial")
                 self.dlg.val2_dist.setEnabled(True)
                 self.dlg.val2_dist.setVisible(True)
+                self.dlg.val2_dist.setPlaceholderText("Valor final")
         else:
             self.dlg.val1_dist.setEnabled(False)
             self.dlg.val2_dist.setEnabled(False)
@@ -257,16 +318,87 @@ class SpatialInteractionModels:
             if index == 1 or index == 2:
                 self.dlg.val1_fluj.setEnabled(True)
                 self.dlg.val1_fluj.setVisible(True)
+                self.dlg.val1_dist.setPlaceholderText("Ingresa un valor")
                 self.dlg.val2_fluj.setEnabled(False)
                 self.dlg.val2_fluj.setVisible(False)
             elif index == 3:
                 self.dlg.val1_fluj.setEnabled(True)
                 self.dlg.val1_fluj.setVisible(True)
+                self.dlg.val1_dist.setPlaceholderText("Valor inicial")
                 self.dlg.val2_fluj.setEnabled(True)
                 self.dlg.val2_fluj.setVisible(True)
+                self.dlg.val2_dist.setPlaceholderText("Valor final")
         else:
             self.dlg.val1_fluj.setEnabled(False)
             self.dlg.val1_fluj.setVisible(False)
             self.dlg.val2_fluj.setEnabled(False)
             self.dlg.val2_fluj.setVisible(False)
+
+    def hide_show_sqlite(self, state):
+        if state:
+            self.dlg.sqlite_check_load.setVisible(True)
+        else:
+            self.dlg.sqlite_check_load.setVisible(False)
+            self.dlg.sqlite_check_load.setChecked(False)
+
+    def hide_show_geojson(self,state):
+        if state:
+            self.dlg.geojson_check_load.setVisible(True)
+        else:
+            self.dlg.geojson_check_load.setVisible(False)
+            self.dlg.geojson_check_load.setChecked(False)
+
+    def hide_show_geopackage(self,state):
+        if state:
+            self.dlg.geopackage_check_load.setVisible(True)
+        else:
+            self.dlg.geopackage_check_load.setVisible(False)
+            self.dlg.geopackage_check_load.setChecked(False)
+
+    def hide_show_hd(self,state):
+        if state:
+            self.dlg.hd_check_load.setVisible(True)
+        else:
+            self.dlg.hd_check_load.setVisible(False)
+            self.dlg.hd_check_load.setChecked(False)
+
+    def recent_projects(self,state):
+        if state:
+            self.dlg.projects_combobox.setEnabled(True)
+        else:
+            self.dlg.projects_combobox.setCurrentIndex(0)
+            self.dlg.projects_combobox.setEnabled(False)
+
+    def load_projects(self):
+        settings = QSettings()
+        pjts = {}
+        self.dlg.projects_combobox.addItem("Seleciona un proyecto...",None)
+        for i in range(1,1000):
+            rpt = settings.value(f'UI/recentProjects/{i}/title')
+            rpp = settings.value(f'UI/recentProjects/{i}/path')
+            if rpt == None:
+                break
+            else:
+                pth = rpp.split(f"{rpt}.qgz",1)[0]
+                pjts[str(i)]={"PROJECT":rpt,"PATH":pth}
+
+        for i in range(1, len(pjts)+1):
+            self.dlg.projects_combobox.addItem(pjts[str(i)]["PROJECT"],pjts[str(i)]["PATH"])
+
+    def set_project_output(self):
+        index = self.dlg.projects_combobox.currentIndex()
+        if index != 0:
+            pth = self.dlg.projects_combobox.itemData(index)
+            self.dlg.output.setText(pth)
+        else:
+            self.dlg.output.clear()
+
+    def select_output(self):
+        self.dlg.output.clear()
+        path = str(QFileDialog.getExistingDirectory(self.dlg, "Seleccionar ruta de salida"))
+        if path != "":
+            self.dlg.output.setText(path+"/")
+        else:
+            print("Error - Selecciona una carpeta")
+
 
